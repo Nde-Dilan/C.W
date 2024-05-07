4539c787b76d5a32c6d03c76782405975f6050e6
Nde Dilan <nde.dilan@ictuniversity.edu.cm>
2024-05-07 14:46:28 +0100

    test  test

--- cw-frontend/components/Post.jsx
+++ cw-frontend/components/Post.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Post = () => {
   return (
-    <div>Post</div>
+    <section>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <p>description</p>
+          </div>
+        </div>
+
+        <div className="post">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, culpa. Quaerat, magni. Cum, atque? Molestiae, vitae explicabo, nesciunt similique numquam est impedit dignissimos beatae, quos accusantium animi. Fugiat praesentium molestiae, dolorum doloribus repellendus labore reprehenderit. Temporibus repellendus, saepe est, aliquam sapiente earum aperiam ullam fugit maxime dolores praesentium architecto consequatur?
+        </div>
+      </div>
+
+    </section>
   )
 }
 
--- cw-frontend/src/main.jsx
+++ cw-frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Post from '../components/Post.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Post />
   </React.StrictMode>,
 )
